81253ce98edaa813fc851906acc68c92a2d72204
TLH-DRTY <talhadartay@gmail.com>
2019-12-12 22:10:35 +0300

    fonk ss
    sssf

--- fonktion/main.c
+++ fonktion/main.c
@@ -18,7 +18,7 @@ int addNumbers(int a, int b)         // function definition
 {
     int result;
     result = a+b;
-    return result;                  // return statement
+    return result;                  
 }
 */
 
